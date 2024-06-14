511188b7c7dc9e992c210b7939640cb8b3433b61
Kh Dev <brahim.khaouni07@gmail.com>
2024-06-14 10:39:32 +0200

    Add files via upload

---
+++ 006 Insert-Add Data Student/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Net;
+using System.Data;
+using System.Data.SqlClient;
+
+public class Program
+{
+    static string connectionstring = "Server=.;database=StudentsDB;user =sa;password=kaka2020";
+
+
+    public static void AddNewStudents(stStudent Student)
+    {
+        SqlConnection connection = new SqlConnection(connectionstring);
+       
+        string qurey = @"insert into Students (FirstName,LastName,Email,Phone,UniversityMajor)
+                        values (@FirstName,@LastName,@Email,@Phone,@UniversityMajor) ";
+
+        SqlCommand command = new SqlCommand(qurey, connection);
+        command.Parameters.AddWithValue("@FirstName", Student.FirstName);
+        command.Parameters.AddWithValue("@LastName", Student.LastName);
+        command.Parameters.AddWithValue("@Email", Student.Email);
+        command.Parameters.AddWithValue("@Phone", Student.Phone);
+        command.Parameters.AddWithValue("@UniversityMajor", Student.UniversityMajor);
+
+
+        try
+        {
+            connection.Open();
+            int AddRow = command.ExecuteNonQuery();
+            if (AddRow > 0)
+            {
+                Console.WriteLine("Is Done");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+
+            connection.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+
+
+
+
+
+    public struct stStudent
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string UniversityMajor { get; set; }
+
+
+    }
+    public static stStudent ReadNewStudent()
+    {
+        stStudent student = new stStudent();
+
+        Console.WriteLine("Enter Your FirstName");
+        student.FirstName = Console.ReadLine();
+
+        Console.WriteLine("Enter Your LastName");
+        student.LastName = Console.ReadLine();
+
+        Console.WriteLine("Enter Your Email");
+        student.Email = Console.ReadLine();
+
+        Console.WriteLine("Enter Your Phone");
+        student.Phone = Console.ReadLine();
+
+        Console.WriteLine("Enter Your UniversityMajor");
+        student.UniversityMajor = Console.ReadLine();
+
+        return student;
+    }
+    public static void Main()
+    {
+
+
+
+        
+        stStudent student = ReadNewStudent();
+        AddNewStudents(student);
+        Console.ReadKey();
+    }
+}
---
+++ 009  Delete Data Student/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Data;
+using System.Data.SqlClient;
+
+public class Program
+{
+    static string connectionstring = "Server=.;database=StudentsDB;user =sa;password=kaka2020";
+
+
+    public static void DeleteStudent(int Student_ID)
+    {
+        SqlConnection connection = new SqlConnection(connectionstring);
+
+        string qurey = "delete from Students  where ID = @ID";
+
+        SqlCommand command = new SqlCommand(qurey, connection);
+
+        command.Parameters.AddWithValue("@ID", Student_ID);
+
+
+
+        try
+        {
+            connection.Open();
+            int AddRow = command.ExecuteNonQuery();
+            if (AddRow > 0)
+            {
+                Console.WriteLine("Is Done");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+
+            connection.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    public static void Main()
+    {
+
+        DeleteStudent(64);
+
+        Console.ReadKey();
+    }
+}
